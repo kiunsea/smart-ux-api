d52bb7f02da1321a01fd2545a8c1f6012f4eb218
lee.kiun <74360831+kiunsea@users.noreply.github.com>
2025-04-15 23:18:57 +0900

    Task #832 작업중 - ChatMode, ActionMode   - 내장형의 servlet class 는 포기한다

--- smart-ux-api/lib/src/main/java/com/omnibuscode/ai/ChatServletBack.java
+++ smart-ux-api/lib/src/main/java/com/omnibuscode/ai/ChatServletBack.java
@@ -6,10 +6,6 @@ import java.io.PrintWriter;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.omnibuscode.ai.ChatManager;
-import com.omnibuscode.ai.ChatRoom;
-import com.omnibuscode.ai.ProcessFunction;
 import com.omnibuscode.ai.openai.Assistant;
 
 import jakarta.servlet.ServletException;
@@ -20,14 +16,45 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 /**
- * 사용자 발화를 처리하기 위한 servlet
+ * 2025.04.15 사용 안하기로 결정했다.
+ * 어차피 설계 복잡도만 높아질뿐 효용성이 그리 높아 보이지 않아서이다.
+ * 그냥 기존처럼 패키지의 클래스들을 이용해서 ai response message 를 취하고
+ * 직접 처리하면 그게 더 간단해 보인다.
+ * 이 클래스는 당분간 유지하다가 코드 재사용도 없어 보이면 삭제하도록 한다.
+ * 
+ * ========================================================
+ * 사용자 발화를 처리하기 위한 Servlet
+ * 이 클래스를 상속받아 super.doPost() 를 호출하게 되면 AI에게 발화하고 응답을 받을 수 있다.
+ * defaultPrint 값을 true로 설정하면 기본 출력이 가능하며
+ * doPost() 함수 호출 이후 ChatManager, ChatRoom, Assistant Message 를 취할 수 있다. 
  * @author KIUNSEA
  */
 @WebServlet("/chat")
-public class ChatServlet extends HttpServlet {
+public class ChatServletBack extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
+    private String openaiAssistId = null;
+    private String openaiApiKey = null;
+    
+    private ChatManager cm = null;
+    private ChatRoom cr = null;
+    private String assistMsg = null;
+    
+    private boolean defaultPrint = false;
+    
+    public void setOpenaiAssistId(String id) {
+    	this.openaiAssistId = id;
+    }
+    
+    public void setOpenaiApiKey(String key) {
+    	this.openaiApiKey = key;
+    }
+    
+	public void setDefaultPrint(boolean defPrint) {
+		this.defaultPrint = defPrint;
+	}
+    
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         doGet(req, res);
@@ -37,53 +64,65 @@ public class ChatServlet extends HttpServlet {
         
     	JSONObject resObj = new JSONObject();
 
-    	ChatManager cm = new ChatManager();
-		ChatRoom cr = null;
-		
-		HttpSession sess = req.getSession(true);
-		Object usObj = sess.getAttribute("CHAT_ROOM");
-		if (usObj != null) {
-			cr = (ChatRoom) usObj;
-		}
-		if (cr == null) {
-			String assistantId = "OPENAI_ASSIST_ID"; //TODO 키값을 가져오게 해야 한다.(하드코딩은 X)
-			Assistant assist = new Assistant(assistantId);
-			String apiKey = "OPENAI_API_KEY";
-			assist.setApiKey(apiKey);
+		if (this.openaiAssistId == null || this.openaiApiKey == null) {
+			resObj.put("sys_msg", "ai 사용권한이 없습니다.");
+		} else {
+			this.cm = new ChatManager();
 
-			cm.setAssistant(assist); //assistant 등록
-			JSONObject jo;
-			try {
-				jo = cm.createChatRoom(ChatManager.AI_NAME_OPENAI);
-				cr = (ChatRoom) jo.get("instance");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			HttpSession sess = req.getSession(true);
+			Object usObj = sess.getAttribute("CHAT_ROOM");
+			if (usObj != null) {
+				this.cr = (ChatRoom) usObj;
 			}
-			sess.setAttribute("CHAT_ROOM", cr);
+			if (this.cr == null) {
+
+				Assistant assist = new Assistant(this.openaiAssistId);
+				assist.setApiKey(this.openaiApiKey);
+
+				this.cm.setAssistant(assist); // assistant 등록
+				JSONObject jo;
+				try {
+					jo = this.cm.createChatRoom(ChatManager.AI_NAME_OPENAI);
+					this.cr = (ChatRoom) jo.get("instance");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				sess.setAttribute("CHAT_ROOM", this.cr);
+			}
+
+			String userMsg = req.getParameter("user_msg");
+			String assistMsg = null;
+			JSONObject resJson;
+			try {
+				resJson = this.cr.sendMessage((userMsg != null ? userMsg : ""));
+				assistMsg = resJson.get("message").toString();
+			} catch (IOException | ParseException e) {
+				e.printStackTrace();
+			} // 메세지 전송
+
+			resObj.put("assist_msg", assistMsg);
 		}
     	
-		String userMsg = req.getParameter("user_msg");
-		String resMsg = null;
-		JSONObject resJson;
-		try {
-			resJson = cr.sendMessage((userMsg != null ? userMsg : ""));
-			resMsg = resJson.get("message").toString();
-		} catch (IOException | ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} //메세지 전송
-		
-		resObj.put("assist_msg", resMsg);
-    	if (resObj == null) resObj = new JSONObject();
-    	
-    	res.setCharacterEncoding("UTF-8");
-        res.setContentType("application/json;charset=UTF-8");
-        res.setHeader("Cache-Control", "no-cache");
-        PrintWriter out = res.getWriter();
-        out.println(resObj);
-        out.flush();
-        out.close();
+		if (this.defaultPrint) {
+			res.setCharacterEncoding("UTF-8");
+			res.setContentType("application/json;charset=UTF-8");
+			res.setHeader("Cache-Control", "no-cache");
+			PrintWriter out = res.getWriter();
+			out.println(resObj);
+			out.flush();
+			out.close();
+		}
     }
 
+	public ChatManager getCm() {
+		return this.cm;
+	}
+
+	public ChatRoom getCr() {
+		return this.cr;
+	}
+
+	public String getAssistMsg() {
+		return this.assistMsg;
+	}
 }
