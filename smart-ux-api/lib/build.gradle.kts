53669ae4ca6294628266eb62bf75b7d3e0ff29dd
lee.kiun <74360831+kiunsea@users.noreply.github.com>
2025-07-07 03:19:52 +0900

    build.gradle.kts 업데이트

--- smart-ux-api/lib/build.gradle.kts
+++ smart-ux-api/lib/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.smartuxapi.ai"  // 그룹 ID 설정
-version = "0.1.0"      // 프로젝트 버전 설정
+version = "0.1.1"      // 프로젝트 버전 설정
 
 repositories {
     // Use Maven Central for resolving dependencies.
